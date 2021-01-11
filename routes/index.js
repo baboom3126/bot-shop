57dd85c7c70d2b5071173f704dbc01b6efecebe3
baboom3126 <km101km@gmail.com>
2021-01-11 17:37:23 +0800

    manager UI template done

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,32 @@ router.get('/demo', function(req, res, next) {
   res.render('demo', { title: 'Express' });
 });
 
+router.get('/member', function(req, res, next) {
+  res.render('member', { title: 'Express' });
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('product', { title: 'Express' });
+});
+
+router.get('/order', function(req, res, next) {
+  res.render('order', { title: 'Express' });
+});
+
+router.get('/LineFriend', function(req, res, next) {
+  res.render('LineFriend', { title: 'Express' });
+});
+
+router.get('/LineSendMsg', function(req, res, next) {
+  res.render('LineSendMsg', { title: 'Express' });
+});
+
+router.get('/LineMsgHistory', function(req, res, next) {
+  res.render('LineMsgHistory', { title: 'Express' });
+});
+
+router.get('/LineBotSetting', function(req, res, next) {
+  res.render('LineBotSetting', { title: 'Express' });
+});
+
 module.exports = router;
